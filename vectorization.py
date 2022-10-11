a6a510bded743dc7cac52d071e4978961637a079
Mitchell P. Krawiec-Thayer <mitchellpkt+github@protonmail.com>
2022-10-11 14:23:36 -0600

    chore: demo numpy vectorize

---
+++ vectorization.py
@@ -0,0 +1,12 @@
+from typing import List
+
+from numpy import vectorize
+
+
+@vectorize
+def capslock(message: str) -> str:
+    return message.upper()
+
+
+list_of_messages: List[str] = ['foo', 'bar']
+print(list(capslock(list_of_messages)))
